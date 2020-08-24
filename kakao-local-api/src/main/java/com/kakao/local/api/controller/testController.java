e05f3bdd616e6146ffdd5ceb4bade3d8b45fa9a9
이유나 <yuna0560@naver.com>
2020-08-24 20:04:45 +0900

    initial commit

--- kakao-local-api/src/main/java/com/kakao/local/api/controller/testController.java
+++ kakao-local-api/src/main/java/com/kakao/local/api/controller/testController.java
@@ -4,21 +4,15 @@ import org.springframework.boot.SpringApplication;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Slf4j
-@EnableWebMvc
-@SpringBootApplication
+@Controller
 public class testController {
 
+	// 주소 받아서 kakao api 결과 응답 
 	public static void main(String[] args) {
-        SpringApplication.run(testController.class, args);
-
-        log.info("========================================================");
-        log.info("=                                                      =");
-        log.info("=                testController		                 =");
-        log.info("=                                                      =");
-        log.info("========================================================");
+        
 
     }
 }
