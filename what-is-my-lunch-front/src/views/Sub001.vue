d58cc42cca32dab40fa630598d451305bae3f16b
이유나 <yuna0560@naver.com>
2020-11-19 21:54:46 +0900

    지도 api 주석 처리

--- what-is-my-lunch-front/src/views/Sub001.vue
+++ what-is-my-lunch-front/src/views/Sub001.vue
@@ -46,24 +46,24 @@ export default {
     methods: {
         home() {
             this.$router.push('/');
-        },
-         addKakaoMapScript() {
-            const script = document.createElement("script");
-            /* global kakao */
-            script.onload = () => kakao.maps.load(this.initMap);
-            script.src =
-                // "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=" + constants1.APIKEY;
-            document.head.appendChild(script);
-        },
-        initMap() {
-            var container = document.getElementById("map_box"); //지도를 담을 영역의 DOM 레퍼런스
-            var options = {
-                //지도를 생성할 때 필요한 기본 옵션
-                center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-                level: 3 //지도의 레벨(확대, 축소 정도)
-            };
-            var map_box = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
         }
+        //  addKakaoMapScript() {
+        //     const script = document.createElement("script");
+        //     /* global kakao */
+        //     script.onload = () => kakao.maps.load(this.initMap);
+        //     script.src =
+        //         "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=" + constants1.APIKEY;
+        //     document.head.appendChild(script);
+        // },
+        // initMap() {
+        //     var container = document.getElementById("map_box"); //지도를 담을 영역의 DOM 레퍼런스
+        //     var options = {
+        //         //지도를 생성할 때 필요한 기본 옵션
+        //         center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+        //         level: 3 //지도의 레벨(확대, 축소 정도)
+        //     };
+        //     var map_box = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+        // }
     }
 };
 </script>
